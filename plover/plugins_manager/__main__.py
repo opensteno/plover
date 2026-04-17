ac6a9a3a6db399ae01fe795c97ac43ff22919ca8
greenwyrt <117969582+greenwyrt@users.noreply.github.com>
2026-04-16 22:20:48 -0700

    fixes #1780

--- plover/plugins_manager/__main__.py
+++ plover/plugins_manager/__main__.py
@@ -82,7 +82,7 @@ def list_plugins(freeze=False):
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    if args[0] == "list_plugins":
+    if args and args[0] == "list_plugins":
         assert len(args) <= 2
         if len(args) > 1:
             assert args[1] == "--freeze"
